26302aea7744ce65a38b1d6c3332bfcb08c79806
dbrackovic2 <dbrackovic2@etf.unsa.ba>
2018-07-27 12:01:02 +0200

    add documentation

---
+++ deltaSkripta.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+folder1="$1/"
+folder2="$2/"
+folder3="$3"
+for x in `rsync -rcnC --out-format="%n"  $folder1 $folder2`
+    do
+        if [ -d "$folder1/$x" ]; then
+            mkdir -p "$folder3/$folder1/$x"
+        else
+            cp -frv --parents  $folder1/$x $folder3
+        fi
+done
